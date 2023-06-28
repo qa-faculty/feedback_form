b644888fe85e73bf4c9d6a14162a486d8f71be91
ivannetreba <89270145+ivannetreba@users.noreply.github.com>
2023-06-29 01:07:22 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-document.querySelector('.button input').addEventListener('click', function(e) {
+document.querySelector('form').addEventListener('submit', function(e) {
   e.preventDefault();
+  
+  // Этот код показывает блок с сообщением
   document.getElementById('messageBanner').style.display = 'block';
+  
+  // Этот код скрывает форму
+  this.style.display = 'none';
 });
+
