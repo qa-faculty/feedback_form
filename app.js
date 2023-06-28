9641455cd60fe6281a79684d47fc20749328454f
ivannetreba <89270145+ivannetreba@users.noreply.github.com>
2023-06-29 01:42:00 +0400

    Попытка исправить отсутствие отправки app.js

--- app.js
+++ app.js
@@ -13,54 +13,22 @@ document.querySelector('.button input[type="submit"]').addEventListener('click',
   var phoneValue = phoneInput.value.trim();
   var messageValue = messageInput.value.trim();
 
-  // Валидация поля "Имя"
-  if (nameValue === '') {
-    alert('Пожалуйста, введите ваше имя.');
-    nameInput.focus();
-    return;
-  }
-  if (nameValue !== '' && !/^[a-zA-Zа-яА-Я\s-]{2,30}$/.test(nameValue)) {
-    alert('Пожалуйста, введите корректное имя. Допустимы только буквы, пробелы и дефисы.');
-    nameInput.focus();
-    return;
-  }
-
-  // Валидация поля "Email"
-  if (emailValue === '') {
-    alert('Пожалуйста, введите ваш email.');
-    emailInput.focus();
-    return;
-  }
-  if (emailValue !== '' && !/\S+@\S+\.\S+/.test(emailValue)) {
-    alert('Пожалуйста, введите корректный email.');
-    emailInput.focus();
-    return;
-  }
-
-  // Валидация поля "Телефон"
-  if (phoneValue === '') {
-    alert('Пожалуйста, введите ваш номер телефона.');
-    phoneInput.focus();
-    return;
-  }
-  if (phoneValue !== '' && !/^\d+$/.test(phoneValue)) {
-    alert('Пожалуйста, введите корректный номер телефона. Допустимы только цифры.');
-    phoneInput.focus();
-    return;
-  }
-
-  // Валидация поля "Сообщение"
-  if (messageValue === '') {
-    alert('Пожалуйста, введите сообщение.');
-    messageInput.focus();
-    return;
-  }
-  if (messageValue.length < 10 || messageValue.length > 500) {
-    alert('Длина сообщения должна быть от 10 до 500 символов.');
-    messageInput.focus();
-    return;
-  }
+  // Валидация полей формы...
 
   // Все поля прошли валидацию, можно отправлять форму
   // В данном коде отправка формы не реализована, нужно добавить соответствующий код для отправки данных на сервер или обработки формы.
+
+});
+
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  // Этот код показывает блок с сообщением
+  var messageBanner = document.getElementById('messageBanner');
+  if (messageBanner) {
+    messageBanner.style.display = 'block';
+  }
+  
+  // Этот код скрывает форму
+  this.style.display = 'none';
 });
